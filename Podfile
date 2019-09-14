0f3e716fe77d3a133c44fe570290940e9291998b
Valentin Shamardin MacBook Pro <poglicier@gmail.com>
2019-09-14 09:04:15 +0300

    Добавил проект

---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '12.0'
+
+target 'TsumRx' do
+  use_frameworks!
+
+  pod 'Moya'
+  pod 'RxSwift'
+  pod 'RxCocoa'
+end
+
+target 'TsumRxTests' do
+  use_frameworks!
+end
+
+
---
+++ TsumRx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TsumRx
+//
+//  Created by Валентин Шамардин on 14/09/2019.
+//  Copyright © 2019 Валентин Шамардин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
